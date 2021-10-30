e18d6b35add29e8cba345509cd794106205e00c6
phamtienphuc1102 <64759245+phamtienphuc1102@users.noreply.github.com>
2021-10-30 16:06:28 +0700

    Update ex.java

--- ex.java
+++ ex.java
@@ -2,10 +2,10 @@ import static java.lang.StrictMath.sqrt;
 
 public class ex {
     public static String tuongquan2dt(int x1, int y1, int r1, int x2, int y2, int r2) {
-        int r3 = Math.abs(r1-r2); // giá trị tuyệt đối hiệu 2 bán kính
-        int r4 = (r1+r2); // tổng 2 bán kính
-
-        double d = sqrt ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); // khoảng cách 2 tâm đường tròn
+        int r3 = Math.abs(r1-r2); 
+        int r4 = (r1+r2);
+        double d = sqrt ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); 
+        
         if(r1<=0 || r2<=0){
             return "input không hợp lệ";
         } else {
@@ -19,7 +19,7 @@ public class ex {
                 return "giao nhau";
             } else if(r3==d || d==r4) {
                 return "tiếp xúc";
-            }else { // d>r4
+            }else { 
                 return "không giao nhau";
             }
         }
